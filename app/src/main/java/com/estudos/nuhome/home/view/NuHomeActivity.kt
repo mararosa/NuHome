9d457ffa2dc1b5ffe61256c12956a8bf0ba38cd4
Mara <edimara.rosa@hotmail.com>
2022-07-06 01:34:27 -0300

    add live data

--- app/src/main/java/com/estudos/nuhome/home/view/NuHomeActivity.kt
+++ app/src/main/java/com/estudos/nuhome/home/view/NuHomeActivity.kt
@@ -9,21 +9,14 @@ package com.estudos.nuhome.home.view
 import android.content.Intent
 import android.os.Bundle
 import android.provider.Settings
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.estudos.nuhome.databinding.ActivityNuHomeBinding
-import com.estudos.nuhome.home.domain.NuHomeInteractor
 import com.estudos.nuhome.home.domain.NuHomeInteractorImpl
 import com.estudos.nuhome.home.domain.NuHomeVO
-import com.estudos.nuhome.home.domain.mapper.toVO
 import com.estudos.nuhome.home.viewmodel.NuHomeViewModel
 import com.estudos.nuhome.home.viewmodel.NuHomeViewModelFactory
-import com.estudos.nuhome.service.ServiceImpl
-import com.estudos.nuhome.service.response.HomeUserDetailsResponse
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class NuHomeActivity : AppCompatActivity() {
 
@@ -33,7 +26,11 @@ class NuHomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         val viewModelFactory = NuHomeViewModelFactory(interactor = NuHomeInteractorImpl())
-        val viewModel = ViewModelProviders.of(this, viewModelFactory).get(NuHomeViewModel::class.java)
+        val viewModel =
+            ViewModelProviders.of(this, viewModelFactory).get(NuHomeViewModel::class.java)
+
+        /** Setting up LiveData observation relationship **/
+        viewModel.infoVO.observe(this, Observer { setupView(it) })
 
         binding = ActivityNuHomeBinding.inflate(layoutInflater)
         binding.viewmodel = viewModel
@@ -45,13 +42,13 @@ class NuHomeActivity : AppCompatActivity() {
     }
 
 
-//    private fun setupView(vo: NuHomeVO?) {
-//        vo?.let {
-//            binding.brandIcon.setImageResource(it.creditCardIcon)
-//        }
-//        binding.userName.text = vo?.userName
-//        binding.balanceAvailableValue.text = vo?.totalAmoutAvailable
-//    }
+    private fun setupView(vo: NuHomeVO?) {
+        vo?.let {
+            binding.brandIcon.setImageResource(it.creditCardIcon)
+        }
+        binding.userName.text = vo?.userName
+        binding.balanceAvailableValue.text = vo?.totalAmoutAvailable
+    }
 
     private fun setupClickListeners() {
         binding.buttonSettings.setOnClickListener {
--- app/src/main/java/com/estudos/nuhome/home/viewmodel/NuHomeViewModel.kt
+++ app/src/main/java/com/estudos/nuhome/home/viewmodel/NuHomeViewModel.kt
@@ -3,6 +3,7 @@ package com.estudos.nuhome.home.viewmodel
 import android.util.Log
 import android.widget.Toast
 import androidx.databinding.BindingAdapter
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.estudos.nuhome.R
 import com.estudos.nuhome.home.domain.NuHomeInteractor
@@ -16,11 +17,7 @@ import retrofit2.Response
 class NuHomeViewModel(
     private val interactor: NuHomeInteractor
 ) : ViewModel() {
-    var infoVO: NuHomeVO? = NuHomeVO(
-        userName = "",
-        totalAmoutAvailable = "",
-        creditCardIcon = R.drawable.icon_card_unknown
-    )
+    var infoVO = MutableLiveData<NuHomeVO>()
 
     //Add live data para a tela ouvir a info que chega da request
     fun loadInfo() {
@@ -30,7 +27,7 @@ class NuHomeViewModel(
                 call: Call<HomeUserDetailsResponse>,
                 response: Response<HomeUserDetailsResponse>
             ) {
-                infoVO = response.body()?.toVO()
+                infoVO.value = response.body()?.toVO()
             }
 
             override fun onFailure(call: Call<HomeUserDetailsResponse>, t: Throwable) {
